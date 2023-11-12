48b0c9763d6cfbbffa0ebdfc36a7f83ef934b39b
EunjinWoo <esther0904@naver.com>
2023-11-12 21:25:32 +0900

    Feat: Recipe Create 페이지

--- src/pages/recipe/CreatePage.jsx
+++ src/pages/recipe/CreatePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import CreateForm from '../../component/CreateForm'
 
 export default function CreatePage() {
   return (
     <div>
       CreatePage
+      <CreateForm />
     </div>
   )
 }
