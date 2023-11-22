fd462eca1ddb823adb4121c76440cdf9852f2753
hyun <duboo31@gmail.com>
2023-11-23 03:29:55 +0900

    Feat: 비밀번호 재설정 기능 구현 완료

---
+++ src/component/user/AuthTimer.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+const AuthTimer = () => {
+  const [minutes, setMinutes] = useState(3);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (parseInt(seconds) > 0) {
+        setSeconds(parseInt(seconds) - 1);
+      }
+      if (parseInt(seconds) === 0) {
+        if (parseInt(minutes) === 0) {
+          clearInterval(countdown);
+        } else {
+          setMinutes(parseInt(minutes) - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
+
+  return (
+    <div>
+      {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+    </div>
+  );
+};
+
+export default AuthTimer;
