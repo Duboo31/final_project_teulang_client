c760dc4b21c58421aea6fed00342c01ce04abb19
EunjinWoo <esther0904@naver.com>
2023-11-25 22:04:06 +0900

    Feat: Search Image modal layout

--- src/component/SearchImageModal.jsx
+++ src/component/SearchImageModal.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
-import "../styles/BookmarksModal.css";
 import axios from "../api/recipes/axios";
 import { useNavigate } from "react-router-dom";
+import "../styles/SearchImageModal.css"
 
-export default function SearchImageModal({ setModalOpen, searchValue, setIsNaviActive }) {
+export default function SearchImageModal({
+  setModalOpen,
+  searchValue,
+  setIsNaviActive,
+}) {
   const [searchImage, setSearchImage] = useState(null);
   const [detectedIngresStr, setDetectedIngresStr] = useState(null);
   const navigate = useNavigate();
@@ -29,54 +33,55 @@ export default function SearchImageModal({ setModalOpen, searchValue, setIsNaviA
       formData.append("image", searchImage);
 
       await axios
-        .post(
-          "articles/detect_objects/",
-          formData
-        )
+        .post("articles/detect_objects/", formData)
         .then(function (response) {
           console.log("reponse.data ", response.data);
           const detected_classes = response.data.detected_classes;
           let detected_classes_str = "";
-          for (let i=0; i<detected_classes.length; i++) {
-            if (i === detected_classes.length-1) {
-                detected_classes_str += detected_classes[i]
+          for (let i = 0; i < detected_classes.length; i++) {
+            if (i === detected_classes.length - 1) {
+              detected_classes_str += detected_classes[i];
             } else {
-                detected_classes_str += detected_classes[i] + ","
+              detected_classes_str += detected_classes[i] + ",";
             }
           }
           console.log(detected_classes_str);
-          document.getElementById("detected_ingredients").innerText = (detected_classes_str === "" ? "감지된 식재료가 없습니다." : detected_classes_str);
+          document.getElementById("detected_ingredients").innerText =
+            detected_classes_str === ""
+              ? "감지된 식재료가 없습니다."
+              : detected_classes_str;
           setDetectedIngresStr(detected_classes_str);
         })
         .catch(function (error) {
           console.log(error);
         });
-    }
-    else {
-        alert("감지할 이미지를 넣어주세요.")
+    } else {
+      alert("감지할 이미지를 넣어주세요.");
     }
   };
 
   const handleSearchIngredients = () => {
     console.log("detectedIngres", detectedIngresStr);
-    if (detectedIngresStr !== null) { // null은 아니고, 제출하긴 한 상태.
-        if (detectedIngresStr === "") {
-            if (window.confirm("감지된 식재료가 없습니다. 그래도 검색하시겠습니까?")) {
-                searchValue = detectedIngresStr;
-                navigate(`/search?q=${searchValue}&page=1`);
-                setIsNaviActive((cur) => !cur);
-                setModalOpen(false);
-            }
-        } 
-        else {
-            searchValue = detectedIngresStr;
-            navigate(`/search?q=${searchValue}&page=1`);
-            setIsNaviActive((cur) => !cur);
-            setModalOpen(false);
+    if (detectedIngresStr !== null) {
+      // null은 아니고, 제출하긴 한 상태.
+      if (detectedIngresStr === "") {
+        if (
+          window.confirm("감지된 식재료가 없습니다. 그래도 검색하시겠습니까?")
+        ) {
+          searchValue = detectedIngresStr;
+          navigate(`/search?q=${searchValue}&page=1`);
+          setIsNaviActive((cur) => !cur);
+          setModalOpen(false);
         }
-    }
-    else { // null. 감지할 식재료 이미지를 제출하지 않은 상태.
-        alert("식재료 이미지를 먼저 감지해주세요.");
+      } else {
+        searchValue = detectedIngresStr;
+        navigate(`/search?q=${searchValue}&page=1`);
+        setIsNaviActive((cur) => !cur);
+        setModalOpen(false);
+      }
+    } else {
+      // null. 감지할 식재료 이미지를 제출하지 않은 상태.
+      alert("식재료 이미지를 먼저 감지해주세요.");
     }
   };
 
@@ -89,11 +94,29 @@ export default function SearchImageModal({ setModalOpen, searchValue, setIsNaviA
           </span>
 
           <div className="modal__content">
-            <input type="file" onChange={onChange} />
-            <img id="search_img_preview" />
-            감지된 식재료: <span id="detected_ingredients"></span>
-            <button onClick={handleDetectIngredients}>감지</button>
-            <button onClick={handleSearchIngredients}>검색</button>
+            <div className="search_modal_content">
+              <div className="search_modal_left">
+                <input
+                  type="file"
+                  onChange={onChange}
+                  id="image_input_for_detection"
+                  style={{ display: "none" }}
+                />
+                <img id="search_img_preview" className="search_img_preview"/>
+                <label htmlFor="image_input_for_detection" className="search_modal_btn">
+                  감지할 사진 등록
+                </label>
+
+                
+              </div>
+              <div className="search_modal_right">
+                <div>감지된 식재료: <span id="detected_ingredients" className="search_modal_detected_ingres"></span></div>
+                <button onClick={handleDetectIngredients} className="search_modal_btn">감지</button>
+              </div>
+            </div>
+            <div className="search_modal_footer">
+              <button onClick={handleSearchIngredients} className="search_modal_btn">검색</button>
+            </div>
           </div>
         </div>
       </div>
