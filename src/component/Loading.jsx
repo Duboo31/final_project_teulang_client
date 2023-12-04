28ccd65cb376fe39d62f365168a977552831a9d2
EunjinWoo <esther0904@naver.com>
2023-12-04 11:10:58 +0900

    Fix: loading 이미지 적용

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import loadingGif from "../images/loading-gif.gif"
 
 export default function Loading() {
   return (
-    <div>
-      Loading ...
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", minHeight: "500px"}}>
+      <img src={loadingGif} style={{width: "150px"}}></img>
     </div>
   )
 }
