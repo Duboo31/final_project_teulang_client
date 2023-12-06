76b2549d267020d0ac22704da88be41d3cc772a8
EunjinWoo <esther0904@naver.com>
2023-12-06 11:16:03 +0900

    Fix: connect to server

--- src/shared/url.js
+++ src/shared/url.js
@@ -1,6 +1,6 @@
 const urls = {
-  // baseURL: "https://www.teulang.shop",
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://www.teulang.shop",
+  // baseURL: "http://127.0.0.1:8000/",
   foodSafetyKoreaURL: "https://www.foodsafetykorea.go.kr",
 };
 
